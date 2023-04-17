a53a983358a6161a5dbe1738223e7ba845144d95
Òscar BarberoPrieto <118044020+DevJava-lab@users.noreply.github.com>
2023-04-17 18:17:03 +0200

    S08 Finalitzat

--- S01T08N02_BarberoPrieto_Oscar/src/Exercici4/OrdenarLlista.java
+++ S01T08N02_BarberoPrieto_Oscar/src/Exercici4/OrdenarLlista.java
@@ -12,29 +12,24 @@ public class OrdenarLlista {
 
 		System.out.println("-> Ordenat alfabèticament pel primer caràcter.\n");
 
-		
 		paraules.stream().sorted(Comparator.comparingInt(p -> p.charAt(0))).forEach(System.out::println);
-		
 
 		System.out.println("\n-> Les cadenes que contenen una \"e\" primer, la resta de cadenes després.\n");
 
-		Map<Boolean, List<String>> subllistes =paraules.stream().sorted().collect(Collectors.partitioningBy(p -> p.contains("e")));
-		
-		List<List<String>> llistaStrings = new ArrayList<>(subllistes.values());
-		 
-        System.out.println("Llista amb \"e\" : " + llistaStrings.get(1));
-        System.out.println("Resta de la llista  : " + llistaStrings.get(0));
+		Map<Boolean, List<String>> subllistes = paraules.stream().sorted()
+				.collect(Collectors.partitioningBy(p -> p.contains("e")));
 
-        
+		List<List<String>> llistaStrings = new ArrayList<>(subllistes.values());
+
+		System.out.println("Llista amb \"e\" : " + llistaStrings.get(1));
+		System.out.println("Resta de la llista  : " + llistaStrings.get(0));
+
 		System.out.println("\n-> Modifica  ‘a’ per un ‘4’.\n");
 
-		
 		paraules.stream().map(p -> p.replace("a", "4")).forEach(System.out::println);
-		
 
 		System.out.println("\n->Mostra només els elements que són numèrics.\n");
 
-		
 		paraules.stream().filter(p -> p.charAt(0) > 48 && p.charAt(0) < 57).forEach(System.out::println);
 
 	}
